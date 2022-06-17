1efbcddb2664ad54c48b8da49329e207caf38bca
juliangauna <julian.gauna@mosaic.com>
2022-06-17 20:08:30 -0300

    Springboot configuration

---
+++ src/main/java/com/flock/provincias/ProvinciasApplication.java
@@ -0,0 +1,13 @@
+package com.flock.provincias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvinciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvinciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flock/provincias/ProvinciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flock.provincias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvinciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
